e6d6481572efeec9ab5b2465e3b7d30c7002e5c9
sarkarghya <sarkarghya@gmail.com>
2020-07-07 17:50:55 +0000

    if

--- main.py
+++ main.py
@@ -11,138 +11,24 @@ the source code is to the left, called loopwhile.py. let me (CJ) know if there a
 from loopwhile import Program
 
 program = """
-
-Y=0
-Y=Y+1
-
-I=0
-J=0
+A=0
+A=A+1
 LOOP X
- I=I+1
- J=J+1
+ A=0
 END
-
-K=0
-S=0
-
-LOOP X
- Y=Y+1
-
- G=0
+LOOP A
  LOOP Y
-  G=G+1
- END 
- G=G+1
-
- LOOP I
-  H = 0
-  LOOP G
-   G = H
-   H = H + 1
-  END
- END
- 
- F=0
- LOOP G
-  F=0
-  F=F+1
- END
-
-
- LOOP F
-  V = 0
-  V=V+1
-  LOOP V
-   H = 0
-   LOOP Y
-    Y = H
-    H = H + 1
-   END
-  END
- END
- 
- X=0
- LOOP I
-  X=X+1
- END
-
- P = 0
- 
- LOOP X
- P = P + 1
- END
- 
- X = X + 1
- Z = 0
- LOOP X
-  LOOP Y
-   A = 0
-   LOOP X
-    X = A
-    A = A + 1
-   END
-  END
- 
-  B = 0
-  LOOP X
-   B = 0
-   B = B + 1
-  END
- 
-  LOOP B
-   Z = Z + 1
-  END
- END 
-
- R = 0
- LOOP Z
-  LOOP Y
-   R = R + 1
-  END
- END
- 
- LOOP R
-  C = 0
-  LOOP P
-   P = C
-   C = C + 1
-  END
- END
- 
-
- S=0
- LOOP P
-  S=0
-  S=S+1
- END
- 
- LOOP S
-  K=K+1
+  B=B+1
  END
 END
-
-LOOP K
- A = 0
- LOOP J
-  J = A
-  A = A+1
- END
-END
-
-E=0
-LOOP J
- E=0
- E=E+1
-END
-
 """
 
 inputs = {
-  "X": 8,
-  "Y": 0,
+  "X": 0,
+  "Y": 5,
 }
 
-output = "E"
+output = "B"
 
 prog = Program(program)
 print(prog.run(inputs, output))
